33ebc62880f060f4129158fbef98e90d59f931e0
rndcasper <rndcasper@gmail.com>
2025-02-01 01:58:04 -0300

    OK

--- aplicacoes/first-application/ec2.tf
+++ aplicacoes/first-application/ec2.tf
@@ -20,6 +20,43 @@ resource "aws_instance" "ec2_brabus_p1" {
 
 }
 
+resource "aws_security_group" "sg_ec2_pub1" {
+  name        = "brabus-sg-ec2-pub1" # Nome não pode iniciar com "sg-"
+  description = "Utilizar para as EC2 publicas"
+  #vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress {
+    description = "Liberar porta 80 para o mundo"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Liberar porta 22 para o mundo"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["179.213.231.5/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name     = "sg-brabus-ec2-pub1"
+    Projeto  = "Brabus"
+    Ambiente = "PRD"
+  }
+}
+
+
+
 /*resource "aws_instance" "ec2_brabus_p2" {
   #count         = "${var.instance_count}"
   ami                         = var.ami
