107bee99b097f7b164fed04f61dba1dcd64c249b
Ronaldo Martins <131787702+rndcasper@users.noreply.github.com>
2025-02-04 13:20:26 -0300

    Update ec2.tf

--- aplicacoes/first-application/ec2.tf
+++ aplicacoes/first-application/ec2.tf
@@ -18,21 +18,6 @@ resource "aws_instance" "ec2_brabus_p1" {
   }
 }
 
-resource "aws_instance" "ec2_brabus_p2" {
-  ami                         = var.ami
-  instance_type               = var.instance_type
-  key_name                    = aws_key_pair.brabus.key_name
-  associate_public_ip_address = true
-  user_data                   = file("./user-data.sh")
-
-  #subnet_id              = data.terraform_remote_state.vpc.outputs.subnet_pub1_id
-  vpc_security_group_ids = [aws_security_group.sg_ec2_pub1.id]
-
-  tags = {
-    Name = "ec2-brabus-p2"
-  }
-}
-
 resource "aws_security_group" "sg_ec2_pub1" {
   name        = "brabus-sg-ec2-pub1" # Nome não pode iniciar com "sg-"
   description = "Utilizar para as EC2 publicas"
